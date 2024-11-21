00fbaa7050b4f7543bc00965be459ddf59785e59
zhouziyu <zhouziyu_bit@outlook.com>
2024-11-22 01:43:26 +0800

    my config

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("lspconfig").clangd.setup({
+  cmd = {"clangd", "-j=8", "--background-index"},
+})
+
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,11 @@ vim.g.autoformat = false
 
 -- 不自动添加eof在文件末尾，从而保持原文件内容
 vim.o.fixendofline = false
+
+-- 关闭跳转过度动画
+vim.g.snacks_animate = false
+
+-- 自动缩进
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
